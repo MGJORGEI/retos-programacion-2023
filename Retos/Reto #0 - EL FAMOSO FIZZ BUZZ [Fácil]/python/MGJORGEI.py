8a81bf4fa50a20b665878ab47f989d80b80763f8
JORGE MONTERO <89808639+MGJORGEI@users.noreply.github.com>
2023-02-23 16:06:04 -0600

    Mis respuestas en retos de programacion
    Estas son mis soluciones para cada reto!

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/python/MGJORGEI.py
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/python/MGJORGEI.py
@@ -1,18 +1,25 @@
-/*
+'''
  * Escribe un programa que muestre por consola (con un print) los
  * números de 1 a 100 (ambos incluidos y con un salto de línea entre
  * cada impresión), sustituyendo los siguientes:
  * - Múltiplos de 3 por la palabra "fizz".
  * - Múltiplos de 5 por la palabra "buzz".
  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
- */
-function fizzBuzz(){
-    for(let i=1;i<101;i++){
-        if(i%3 === 0 && i%5 === 0) console.log('fizzbuzz');
-        else if(i%3 === 0) console.log('fizz');
-        else if(i%5 === 0) console.log('buzz');
-        else console.log(i);
-    }
-}
+'''
 
-fizzBuzz();
+## Solucion implementada por mi 
+
+def fizzBuuz():
+    
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+fizzBuuz()
